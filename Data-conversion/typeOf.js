ffd4df7f4f05f0b27abdc40274a1c2687beac488
Asim-Munnir <141481288+Asim-Munnir@users.noreply.github.com>
2023-12-11 03:13:09 +0000

    conversion of Datatypes

---
+++ Data-conversion/typeOf.js
@@ -0,0 +1,39 @@
+
+// let score = 33
+
+// console.log(typeof score);
+
+let string  = "33abc"
+// console.log(typeof string);
+
+let stringToNumber = Number(string)
+// console.log(stringToNumber);
+// console.log(typeof stringToNumber);
+
+ let score1 = null;
+let score2 = true
+// console.log(score1);
+// console.log(typeof(score2));
+
+// let con = Number(score2);
+
+// console.log(typeof(con))
+let score3 = true
+// console.log(score3);
+let change = Number(score3)
+// console.log(change);
+
+// 33 => 33
+// "33abc" +> NaN
+//  true => 1
+// false => 0
+
+// change the number into boolean
+let isLoggedIn = 0;
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+// console.log(booleanIsLoggedIn); 
+
+let someNumber = 33
+let numberToString = String(someNumber)
+console.log(numberToString);
+console.log(typeof numberToString);
